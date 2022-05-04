f30588f7b774a04bc62e89e92653034773a71803
Kelly McCrow-Young <kelly.ann.tuesday@gmail.com>
2022-05-04 16:26:17 +1200

    Add cell flip logic

--- src/Board.js
+++ src/Board.js
@@ -16,6 +16,7 @@ class Board extends Component {
       board: this.createBoard()
     }
     this.createBoard = this.createBoard.bind(this);
+    this.flipCellsAround = this.flipCellsAround.bind(this);
   }
 
   createBoard() {
@@ -30,41 +31,45 @@ class Board extends Component {
     return board;
   }
 
-  /** handle changing a cell: update board & determine if winner */
-
   flipCellsAround(coord) {
     let {ncols, nrows} = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
 
-
     function flipCell(y, x) {
-      // if this coord is actually on board, flip it
-
       if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
         board[y][x] = !board[y][x];
       }
     }
 
-    // TODO: flip this cell and the cells around it
+    flipCell(y, x);
+    flipCell(y, x - 1);
+    flipCell(y, x + 1);
+    flipCell(y - 1, x);
+    flipCell(y + 1, x);
 
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
-
-    // this.setState({board, hasWon});
   }
 
-
-  /** Render game board or winning message. */
-
   render() {
+    if(this.state.hasWon) {
+      return (
+        <div className='Board-title'>
+          <div className='winner'>
+            <span className='neon-orange'>You</span>
+            <span className='neon-blue'>Win!</span>
+          </div>
+        </div>
+      )
+    }
+
     let tableBoard = [];
-    for(let i = 0; i < this.props.nrows; i ++){
+    for(let y = 0; y < this.props.nrows; y ++){
       let row = [];
       for(let x = 0; x < this.props.ncols; x++){
-        row.push(<Cell isLit={this.state.board[i][x]}/>)
+        let coord = `${y}-${x}`
+        row.push(<Cell key={coord} flipCellsAroundMe={() => this.flipCellsAround(coord)} isLit={this.state.board[y][x]}/>)
       }
-      tableBoard.push(<tr>{row}</tr>)
+      tableBoard.push(<tr key={y}>{row}</tr>)
     }
     return (
       <div>
@@ -75,7 +80,6 @@ class Board extends Component {
         </table>
       </div>
     )
-    // if the game is won, just show a winning msg & render nothing else
   }
 }
 
